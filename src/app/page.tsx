1e82a15fba2d4b327281bc3a39a1d3be5cd8f138
subhrachakraborti <cers.subhra@gmail.com>
2026-05-02 20:31:54 +0530

    Pop-Up hidden for further data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ import {
 
 export default function Home() {
   const chairImg = PlaceHolderImages.find(img => img.id === 'chairperson');
-  const showToppersPopup = true;
+  const showToppersPopup = false; // Set to true to enable the toppers popup
 
   const importantLinks = [
     { title: 'Admissions', icon: <FileText />, href: '/admission' },
